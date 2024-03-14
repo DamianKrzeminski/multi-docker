ff402ed44ec58af8fe36c248b5bbbaa20feb6fd7
DamianKrzeminski <damian.krzeminski13@gmail.com>
2024-03-14 15:03:33 +0000

    ch

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <h1>Fib calculator</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
